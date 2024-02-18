86d9ee532cad903c371641bb9f82c26ba1327745
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-02-18 10:39:53 +0300

    oidc_provider

--- orderv1/settings.py
+++ orderv1/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'oidc_provider',
 ]
 
 ROOT_URLCONF = 'orderv1.urls'
