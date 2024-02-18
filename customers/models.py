01414bd48865e7b061e7041f070ea60d9222b188
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-02-18 10:12:30 +0300

    phone number added in customer model

--- customers/models.py
+++ customers/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=255)
     code = models.CharField(max_length=50, unique=True)
+    phone_number = models.CharField(max_length=15) 
 
     def __str__(self):
         return self.name
