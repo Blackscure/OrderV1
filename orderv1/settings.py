f71aa2361ca6646103c31657bb244a95d94f4309
Blackscure <wekesabuyahi@gmail.com>
2024-02-22 11:43:27 +0300

    urls and settings file modified

--- orderv1/settings.py
+++ orderv1/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'customers.apps.CustomersConfig',
     'orders.apps.OrdersConfig',
+    'oidc_provider',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'oidc_provider',
+    'oidc_provider.middleware.OIDCUserInfoMiddleware',
 ]
 
 ROOT_URLCONF = 'orderv1.urls'
--- orderv1/urls.py
+++ orderv1/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path,url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^oidc/', include('oidc_provider.urls', namespace='oidc_provider')),
     path('apps/api/v1/order/', include('orders.api.urls')),
     path('apps/api/v1/customers/', include('customers.api.urls')),
 ]
